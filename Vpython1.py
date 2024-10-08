157fc27ed221adec2236b77a4acd7420e9743382
Rahmani-851 <ahmedalidsai851@gmail.com>
2024-10-08 19:24:56 +0100

    this is my first commit

---
+++ Vpython1.py
@@ -0,0 +1,7 @@
+from vpython import *
+import vpython
+#ball = vpython.sphere(color = color.red)
+box = vpython.box(color = color.black)
+#button = vpython.button(color = color.blue)
+while True:
+    pass
